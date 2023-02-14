e46d22b53a76153cd4ff0569e1970ab09d487310
Beh <github@beh.wtf>
2023-02-14 22:59:44 +0100

    Add cheesy animations when new track is shown

--- static/script.js
+++ static/script.js
@@ -44,7 +44,6 @@ const animations = [
 const decks = new Map();
 const ws = new WebSocket(`ws://${window.location.host}/ws`);
 let currentTrack = null;
-// let nextTrack = null;
 
 ws.addEventListener("open", () => {
     console.log("WebSocket connected");
@@ -58,45 +57,54 @@ ws.addEventListener("message", (message) => {
     console.log("Got message");
     console.log(track);
 
+    if (currentTrack && currentTrack.title === track.title) {
+      return;
+    } else {
+      currentTrack = JSON.parse(JSON.stringify(track));
+    }
+
     updateScreen(track);
 });
 
-async function updateScreen(track) {
+function updateScreen(track) {
     const currentYear = document.getElementById("current_year");
     const currentTitle = document.getElementById("current_title");
     const currentArtist = document.getElementById("current_artist");
 
-    currentYear.innerHTML = track.year;
-    currentTitle.innerHTML = track.title;
-    currentArtist.innerHTML = track.artist;
+    // currentYear.innerHTML = track.year;
+    // currentTitle.innerHTML = track.title;
+    // currentArtist.innerHTML = track.artist;
 
-    await animatedUpdate(currentYear, track.year);
+    animatedUpdate(currentYear, track.year);
+    animatedUpdate(currentTitle, track.title);
+    animatedUpdate(currentArtist, track.artist);
 }
 
 async function animatedUpdate(element, newText) {
-    const random = Math.random() * (animations.length - 0);
+    const random = Math.floor(Math.random() * (animations.length - 0 + 1)) + 0;
     const animation = animations[random];
+
+    console.log(`Random Animation num ${random}: ${animation}`);
 
     await animateCSS(element, "fadeOut");
     element.innerHTML = newText;
     await animateCSS(element, animation);
 }
 
-const animateCSS = async (element, animation, prefix = "animate__") => {
-    // We create a Promise and return it
-    return new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`;
-        const node = document.querySelector(element);
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = element
 
-        node.classList.add(`${prefix}animated`, animationName);
+    node.classList.add(`${prefix}animated`, animationName);
 
-        // When the animation ends, we clean the classes and resolve the Promise
-        function handleAnimationEnd(event) {
-            event.stopPropagation();
-            node.classList.remove(`${prefix}animated`, animationName);
-            resolve("Animation ended");
-        }
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
 
-        node.addEventListener("animationend", handleAnimationEnd, { once: true });
-    });
-};
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
