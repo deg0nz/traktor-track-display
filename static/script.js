3da5378801fa447b4dc70c071b46a62c834f46d7
Beh <github@beh.wtf>
2023-02-15 11:00:06 +0100

    Add random color themes

--- static/script.js
+++ static/script.js
@@ -40,7 +40,18 @@ const animations = [
     "rollIn",
 ];
 
-const decks = new Map();
+const colorThemes = [
+    "purple-yellow",
+    "black-red",
+    "orange-blue",
+    "orange-blue-inverted",
+    "blue-pink",
+    "blue-pink-inverted",
+    "cyan-pink",
+    "fuchsia-pink",
+    "pink-purple-gradient"
+];
+
 const ws = new WebSocket(`ws://${window.location.host}/ws`);
 let currentTrack = null;
 
@@ -65,7 +76,11 @@ ws.addEventListener("message", (message) => {
     updateScreen(track);
 });
 
-function updateScreen(track) {
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function updateScreen(track) {
     const currentYear = document.getElementById("current_year");
     const currentTitle = document.getElementById("current_title");
     const currentArtist = document.getElementById("current_artist");
@@ -74,18 +89,29 @@ function updateScreen(track) {
     // currentTitle.innerHTML = track.title;
     // currentArtist.innerHTML = track.artist;
 
+    const randomThemeNum = getRandomInt(0, colorThemes.length);
+    const theme = colorThemes[randomThemeNum];
+    const body = document.body;
+
+    await animateCSS(body, "fadeOut");
+    body.classList = theme;
+    currentYear.innerHTML = "";
+    currentTitle.innerHTML = "";
+    currentArtist.innerHTML = "";
+    await animateCSS(body, "fadeIn");
+
     animatedUpdate(currentYear, track.year);
     animatedUpdate(currentTitle, track.title);
     animatedUpdate(currentArtist, track.artist);
 }
 
 async function animatedUpdate(element, newText) {
-    const random = Math.floor(Math.random() * (animations.length - 0 + 1)) + 0;
-    const animation = animations[random];
+    const randomAnimationNum = getRandomInt(0, animations.length);
+    const animation = animations[randomAnimationNum];
 
-    console.log(`Random Animation num ${random}: ${animation}`);
+    console.log(`Random Animation num ${randomAnimationNum}: ${animation}`);
 
-    await animateCSS(element, "fadeOut");
+    // await animateCSS(element, "fadeOut");
     element.innerHTML = newText;
     await animateCSS(element, animation);
 }
